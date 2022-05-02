b16eeb3f0dd7f52cc984aafc8f2bc2e846e776b9
NelsonAlmeida-18 <nelson.martinsalmeidaa@gmail.com>
2022-05-02 13:34:42 +0100

    modified:   src/sdstore.c   "Errors fix, one time exec" 	modified:   src/sdstored.c   "Fixed errors, deleted functions, all on main so it doesnt open and close the descriptor multiple times"

--- src/sdstore.c
+++ src/sdstore.c
@@ -6,6 +6,19 @@
 #include <unistd.h>
 #include <string.h>
 
+int receiveInfo(){
+	int servidor_cliente=open("servidor_cliente_fifo",O_RDONLY,0666);
+	char buffer[1024];
+	int bytesRead;
+	while((bytesRead=read(servidor_cliente,buffer,1024)>0)){
+		write(1,buffer,bytesRead);
+	}
+	if(strcmp(buffer,"Done")==0)
+		_exit(0);
+	close(servidor_cliente);
+	return bytesRead;
+}
+
 
 int makeRequest(int argc, char *argv[]){
 	int cliente_servidor = open("cliente_servidor_fifo", O_WRONLY|O_TRUNC, 0666);
@@ -20,22 +33,18 @@ int makeRequest(int argc, char *argv[]){
 	}
 	write(cliente_servidor, buffer, sizeof(buffer));
 	close(cliente_servidor);
+
+	char string[1024];
+	int bytesRead = 0;
+	int server_client_fifo = open("servidor_cliente_fifo", O_RDONLY);
+	while((bytesRead = read(server_client_fifo, string, 1024)) > 0)
+		write(1, string, bytesRead);
+	close(server_client_fifo);
+
+
 	return 0;
 }
 
-int receiveInfo(){
-	int servidor_cliente=open("servidor_cliente_fifo",O_RDONLY,0666);
-	char buffer[1024];
-	int bytesRead;
-	printf("%s\n", buffer);
-	while((bytesRead=read(servidor_cliente,buffer,sizeof(buffer))>0)){
-		write(1,buffer,sizeof(buffer));
-	}
-	if(strcmp(buffer,"Done")==0)
-		_exit(0);
-	close(servidor_cliente);
-	return bytesRead;
-}
 
 
 int main(int argc, char* argv[]){
@@ -46,10 +55,7 @@ int main(int argc, char* argv[]){
 	}
 
 
-	while(1){
-		makeRequest(argc, argv);
-		receiveInfo();
-	}
+	makeRequest(argc, argv);
 
 	return 0;
 }
