e9d0ceab184a67b047ac27dd888b77d853676348
NelsonAlmeida-18 <nelson.martinsalmeidaa@gmail.com>
2022-05-02 18:43:38 +0100

    modified:   src/sdstore.c   "updates status e queue" 	modified:   src/sdstored.c  "updats status e queue"

--- src/sdstore.c
+++ src/sdstore.c
@@ -7,19 +7,6 @@
 #include <string.h>
 #include <errno.h>
 
-int receiveInfo(){
-	int servidor_cliente=open("servidor_cliente_fifo",O_RDONLY,0666);
-	char buffer[1024];
-	int bytesRead;
-	while((bytesRead=read(servidor_cliente,buffer,1024)>0)){
-		write(1,buffer,bytesRead);
-	}
-	if(strcmp(buffer,"Done")==0)
-		_exit(0);
-	close(servidor_cliente);
-	return bytesRead;
-}
-
 
 int makeRequest(int argc, char *argv[]){
 	int cliente_servidor = open("cliente_servidor_fifo", O_WRONLY|O_TRUNC, 0666);
@@ -41,7 +28,6 @@ int makeRequest(int argc, char *argv[]){
 	while((bytesRead = read(server_client_fifo, string, 1024)) > 0)
 		write(1, string, bytesRead);
 	close(server_client_fifo);
-
 
 	return 0;
 }
--- src/sdstored.c
+++ src/sdstored.c
@@ -57,15 +57,27 @@ int main(int argc, char* argv[]){
 		close(cliente_servidor);
 
 		//char **commands = malloc(sizeof(char)*1024);
-		if(strcmp(buffer, "status")){
-			char* message;
-			for(int i = 0; i<queuesize; i++){
-				write(servidor_cliente, queue[i], sizeof(queue[i])*sizeof(char));
+		if(strncmp(buffer, "status",6)==0){
+			if (queuesize>0){
+				char message[1024]="";
+				for(int i = lastCommands; i<queuesize; i++){
+					int j=0;
+					char messageTemp[1024]="";
+					while(queue[i][j]){
+						strcat(messageTemp,queue[i][j]);
+						j++;
+					}
+					strcat(message,messageTemp);
+				}
+				write(servidor_cliente, message, strlen(message));
+				close(servidor_cliente);
 			}
-
-			return 0;
+			else{
+				char *message="No processes in queue\n";
+				write(servidor_cliente,message , strlen(message));
+			}
 		}
-		else if(strncmp(buffer, "proc-file", 9) == 0){
+		if(strncmp(buffer, "proc-file", 9) == 0){
 			char input[1024];
 			strcpy(input, buffer);
 
@@ -84,66 +96,66 @@ int main(int argc, char* argv[]){
 					size++;
 				}
 			}
-		}
-		queuesize+=1;
-		strcpy(string, "Pending\n");
-		write(servidor_cliente,string, strlen(string));
-
-		//ExecCommands
-
-		if (queuesize>0){
-
-			strcpy(string, "Processing\n");
+			queuesize+=1;
+			strcpy(string, "Pending\n");
 			write(servidor_cliente,string, strlen(string));
 
+			//ExecCommands
 
-			int source = open(queue[0][0], O_RDONLY, 0666);
-			if(source == -1){
-				perror("Erro na origem\n");
-				return 1;
-			}
+			if (queuesize>0){
 
-			int dest = open(queue[0][1], O_WRONLY|O_TRUNC|O_CREAT, 0666);
-			if(dest == -1){
-				perror("Erro no destino\n");
-				return 1;
-			}
+				strcpy(string, "Processing\n");
+				write(servidor_cliente,string, strlen(string));
 
-			//int status;
-			int pipeline[1024][2];
-			
-			for(int i = 2; i < size; i++){
-				if(fork() == 0){
-					if(pipe(pipeline[i-2]) == -1){
-						perror("Erro na criação do pipe\n");
-						return 1;
-					}
 
-					dup2(source, 0);
-					close(source);
-					
-					dup2(dest, 1);
-					close(dest);
-
-					char buffer[100];
-					strcpy(buffer, "../SDStore-transf/");
-					strcat(buffer, queue[0][i]);
-
-					execlp(buffer, buffer, NULL);
-					perror("Erro na execução da funcionalidade\n");
-
-					_exit(0);
+				int source = open(queue[0][0], O_RDONLY, 0666);
+				if(source == -1){
+					perror("Erro na origem\n");
+					return 1;
 				}
+
+				int dest = open(queue[0][1], O_WRONLY|O_TRUNC|O_CREAT, 0666);
+				if(dest == -1){
+					perror("Erro no destino\n");
+					return 1;
+				}
+
+				//int status;
+				int pipeline[1024][2];
+				
+				for(int i = 2; i < size; i++){
+					if(fork() == 0){
+						if(pipe(pipeline[i-2]) == -1){
+							perror("Erro na criação do pipe\n");
+							return 1;
+						}
+
+						dup2(source, 0);
+						close(source);
+						
+						dup2(dest, 1);
+						close(dest);
+
+						char buffer[100];
+						strcpy(buffer, "../SDStore-transf/");
+						strcat(buffer, queue[0][i]);
+
+						execlp(buffer, buffer, NULL);
+						perror("Erro na execução da funcionalidade\n");
+
+						_exit(0);
+					}
+				}
+				lastCommands+=1;
+				strcpy(string,"Done\n");
+				write(servidor_cliente, string, strlen(string));
+
+
+				wait(NULL);
+				close(source);
+				close(dest);
+				close(servidor_cliente);
 			}
-			lastCommands+=1;
-			strcpy(string,"Done\n");
-			write(servidor_cliente, string, strlen(string));
-
-
-			wait(NULL);
-			close(source);
-			close(dest);
-			close(servidor_cliente);
 		}
 	}
 	return 0;
